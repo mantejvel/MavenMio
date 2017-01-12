93c4a8fbc384d46be8f871c5778427e31c056549
mantejvel <manueltejerovela@gmail.com>
2017-01-12 19:47:09 +0100

    Dependencia con jfreechart realizada

--- src/main/java/es/egc/app/App.java
+++ src/main/java/es/egc/app/App.java
@@ -1,5 +1,13 @@
 package es.egc.app;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+
 /**
  * Hello world!
  *
@@ -9,5 +17,32 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		
+		 XYSeries series = new XYSeries("Producto A");
+
+        // Introduccion de datos
+        series.add(1, 1);
+        series.add(2, 6);
+        series.add(3, 3);
+        series.add(4, 10);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Ventas 2014", // Título
+                "Tiempo (x)", // Etiqueta Coordenada X
+                "Cantidad", // Etiqueta Coordenada Y
+                dataset, // Datos
+                PlotOrientation.VERTICAL,
+                true, // Muestra la leyenda de los productos (Producto A)
+                false,
+                false
+        );
+
+        // Mostramos la grafica en pantalla
+        ChartFrame frame = new ChartFrame("Ejemplo Grafica Lineal", chart);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
